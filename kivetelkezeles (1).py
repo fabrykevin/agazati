25fbaa92fdc0216795223aff282dc0d29d237a30
fabrykevin <fabry.kevin@students.jedlik.eu>
2023-06-18 17:02:46 +0200

    c

---
+++ kivetelkezeles (1).py
@@ -0,0 +1,18 @@
+def terulet(a,b):
+    ter=a*b
+    if ter<100:
+        raise ValueError('Hiba: Túl kicsi a telek!')
+    return ter
+
+print("feladat: Terület számítás")
+while True:
+    a=float(input('Kérem adja meg a telek szélességét (a): '))
+    if a==-1:
+        break
+    b=float(input('Kérem adja meg a telek hosszúságát (b): '))
+    if b==-1:
+        break
+    try:
+        print(f'Telek területe: {terulet(a,b)} m2')
+    except ValueError as error:
+        print(error)
